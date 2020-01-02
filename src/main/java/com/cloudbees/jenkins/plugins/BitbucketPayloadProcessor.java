b95f813851960c6e05d01aa752490a8eb5c49797
Yashwant Jagdale <yjagdale@qualys.com>
2020-01-02 22:54:28 +0530

    New Payload support

--- src/main/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessor.java
+++ src/main/java/com/cloudbees/jenkins/plugins/BitbucketPayloadProcessor.java
@@ -29,14 +29,14 @@ public class BitbucketPayloadProcessor {
                 processWebhookPayload(payload);
             }
         } else if (payload.has("actor") && payload.has("repository") && payload.getJSONObject("repository").has("links")) {
-            if ("repo:push".equals(request.getHeader("x-event-key"))) {
+            if ("repo:refs_changed".equals(request.getHeader("x-event-key"))) {
                 LOGGER.log(Level.INFO, "Processing new Webhooks payload");
                 processWebhookPayloadBitBucketServer(payload);
             }
         } else if (payload.has("actor")) {
-        	// we assume that the passed hook was from bitbucket server https://confluence.atlassian.com/bitbucketserver/managing-webhooks-in-bitbucket-server-938025878.html
-        	LOGGER.log(Level.INFO, "Processing webhook for self-hosted bitbucket instance");
-        	processWebhookPayloadBitBucketSelfHosted(payload);
+            // we assume that the passed hook was from bitbucket server https://confluence.atlassian.com/bitbucketserver/managing-webhooks-in-bitbucket-server-938025878.html
+            LOGGER.log(Level.INFO, "Processing webhook for self-hosted bitbucket instance");
+            processWebhookPayloadBitBucketSelfHosted(payload);
         } else {
             LOGGER.log(Level.INFO, "Processing old POST service payload");
             processPostServicePayload(payload);
@@ -45,36 +45,36 @@ public class BitbucketPayloadProcessor {
 
     /**
      * parses the payload from self hosted bitbucket instance which uses the default webhooks implementation.
-     * 
+     *
      * https://confluence.atlassian.com/bitbucketserver0510/managing-webhooks-in-bitbucket-server-951390737.html
      * https://confluence.atlassian.com/bitbucketserver0510/event-payload-951390742.html
-     * 
+     *
      * @param payload The payload matching the definition in https://confluence.atlassian.com/bitbucketserver0510/event-payload-951390742.html
      */
     private void processWebhookPayloadBitBucketSelfHosted(JSONObject payload) {
-    	JSONObject repo;
-    	
-    	// find the repository hidden in different objects
-    	if (payload.has("repository")) { // for push to repository
-    		repo = payload.getJSONObject("repository");
-    	} else if (payload.has("pullRequest")) { // for all PR events
-    		repo = payload.getJSONObject("pullRequest").getJSONObject("toRef").getJSONObject("repository");
-    	} else {
-    		LOGGER.log(Level.WARNING, "Not possible to trigger job for event '{0}'. Only PR events and pushes are supported for now.", payload.get("eventKey"));
-    		LOGGER.log(Level.FINE, payload.toString());
-    		return;
-    	}
-    	
+        JSONObject repo;
+
+        // find the repository hidden in different objects
+        if (payload.has("repository")) { // for push to repository
+            repo = payload.getJSONObject("repository");
+        } else if (payload.has("pullRequest")) { // for all PR events
+            repo = payload.getJSONObject("pullRequest").getJSONObject("toRef").getJSONObject("repository");
+        } else {
+            LOGGER.log(Level.WARNING, "Not possible to trigger job for event '{0}'. Only PR events and pushes are supported for now.", payload.get("eventKey"));
+            LOGGER.log(Level.FINE, payload.toString());
+            return;
+        }
+
         String user = payload.getJSONObject("actor").getString("name");
         String url = repo.getJSONObject("project").getString("key").toLowerCase() + "/" + repo.getString("slug");
 
         // always use git no other repo type supported on self hosted solution
         String scm = "git";
         probe.triggerMatchingJobs(user, url, scm, payload.toString());
-		
-	}
 
-	private void processWebhookPayload(JSONObject payload) {
+    }
+
+    private void processWebhookPayload(JSONObject payload) {
         if (payload.has("repository")) {
             JSONObject repo = payload.getJSONObject("repository");
             LOGGER.log(Level.INFO, "Received commit hook notification for {0}", repo);
@@ -98,12 +98,12 @@ public class BitbucketPayloadProcessor {
     private String getUser(JSONObject payload, String jsonObject) {
         String user;
         try {
-            user = payload.getJSONObject(jsonObject).getString("username");
+            user = payload.getJSONObject(jsonObject).getString("displayName");
         } catch (JSONException e1) {
             try {
-                user = payload.getJSONObject(jsonObject).getString("nickname");
+                user = payload.getJSONObject(jsonObject).getString("slug");
             } catch (JSONException e2) {
-                user = payload.getJSONObject(jsonObject).getString("display_name");
+                user = payload.getJSONObject(jsonObject).getString("name");
             }
         }
         return user;
@@ -123,7 +123,7 @@ public class BitbucketPayloadProcessor {
         if (repo.getJSONObject("links").getJSONArray("self").size() != 0) {
             try {
                 URL pushHref = new URL(repo.getJSONObject("links").getJSONArray("self").getJSONObject(0).getString("href"));
-                url = pushHref.toString().replaceFirst(new String("projects.*"), new String(repo.getString("fullName").toLowerCase()));
+                url = pushHref.toString().replaceFirst(new String("projects.*"), new String(repo.getJSONObject("project").getString("key").toLowerCase() + "/" + repo.getString("slug").toLowerCase()));
                 String scm = repo.has("scmId") ? repo.getString("scmId") : "git";
                 probe.triggerMatchingJobs(user, url, scm, payload.toString());
             } catch (MalformedURLException e) {
@@ -132,45 +132,45 @@ public class BitbucketPayloadProcessor {
         }
     }
 
-/*
-{
-    "canon_url": "https://bitbucket.org",
-    "commits": [
-        {
-            "author": "marcus",
-            "branch": "master",
-            "files": [
-                {
-                    "file": "somefile.py",
-                    "type": "modified"
-                }
-            ],
-            "message": "Added some more things to somefile.py\n",
-            "node": "620ade18607a",
-            "parents": [
-                "702c70160afc"
-            ],
-            "raw_author": "Marcus Bertrand <marcus@somedomain.com>",
-            "raw_node": "620ade18607ac42d872b568bb92acaa9a28620e9",
-            "revision": null,
-            "size": -1,
-            "timestamp": "2012-05-30 05:58:56",
-            "utctimestamp": "2012-05-30 03:58:56+00:00"
-        }
-    ],
-    "repository": {
-        "absolute_url": "/marcus/project-x/",
-        "fork": false,
-        "is_private": true,
-        "name": "Project X",
-        "owner": "marcus",
-        "scm": "git",
-        "slug": "project-x",
-        "website": "https://atlassian.com/"
-    },
-    "user": "marcus"
-}
-*/
+    /*
+    {
+        "canon_url": "https://bitbucket.org",
+        "commits": [
+            {
+                "author": "marcus",
+                "branch": "master",
+                "files": [
+                    {
+                        "file": "somefile.py",
+                        "type": "modified"
+                    }
+                ],
+                "message": "Added some more things to somefile.py\n",
+                "node": "620ade18607a",
+                "parents": [
+                    "702c70160afc"
+                ],
+                "raw_author": "Marcus Bertrand <marcus@somedomain.com>",
+                "raw_node": "620ade18607ac42d872b568bb92acaa9a28620e9",
+                "revision": null,
+                "size": -1,
+                "timestamp": "2012-05-30 05:58:56",
+                "utctimestamp": "2012-05-30 03:58:56+00:00"
+            }
+        ],
+        "repository": {
+            "absolute_url": "/marcus/project-x/",
+            "fork": false,
+            "is_private": true,
+            "name": "Project X",
+            "owner": "marcus",
+            "scm": "git",
+            "slug": "project-x",
+            "website": "https://atlassian.com/"
+        },
+        "user": "marcus"
+    }
+    */
     private void processPostServicePayload(JSONObject payload) {
         JSONObject repo = payload.getJSONObject("repository");
         LOGGER.log(Level.INFO, "Received commit hook notification for {0}", repo);
